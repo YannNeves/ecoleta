db26a10145efd26048711dc7d15dab6d0b8b0fc2
Yann <yannvr@hotmail.com>
2020-10-12 16:06:16 -0300

    remove gitignore files and upload mobile

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,9 +9,11 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `http://localhost:3333/uploads/${item.image}`
+                image_url: `http://192.168.0.102:3333/uploads/${item.image}`
+                
             }
         });
+        
         return response.json(serializedItems);
     }
 }
